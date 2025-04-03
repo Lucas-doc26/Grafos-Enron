5ab461acad60c77e11db6e053e46ed93f9705ae9
TiagoFollador <tiagobritofollador@gmail.com>
2025-04-03 19:46:22 -0300

    Merge Grafo Tigas com Grafo do lucao, e implementado a default dict

--- Grafo.py
+++ Grafo.py
@@ -1,169 +1,161 @@
 import re
 import numpy as np
+from collections import defaultdict
 
 class Grafo:
   def __init__(self):
       self.ordem = 0
       self.tamanho = 0
-      self.corpo = {}
+      self.vertices = []  # Mantém a ordem de inserção
+      self.corpo = defaultdict(list)  # Adjacências com defaultdict
+
+  def __str__(self):
+    return self.imprime_lista()
+    
 
   def get_ordem(self):
     print(f"A ordem do grafo é: {self.ordem}")
     return self.ordem
-  
+
   def get_tamanho(self):
     print(f"O tamanho do grafo é: {self.tamanho}")
     return self.tamanho
 
   def add_vertice(self, nome):
-      if nome not in self.corpo:
-          self.corpo[nome] = []
+      if nome not in self.vertices:
+          self.vertices.append(nome)
           self.ordem += 1
       else:
           raise ValueError("Vértice já existe!")
 
   def imprime_lista(self):
-    for vertice in self.corpo:
-      dados = []
-      if len(self.corpo[vertice]) > 0:
-        for i in range(len(self.corpo[vertice])):
-            dados.append(self.corpo[vertice][i])
-            dados_formatados = [f'{d} ->' for d in dados]
-        print(f'{vertice}:{dados_formatados}')
-      else:
-        print(vertice, ':')
+      lista = ""
+      for vertice in self.vertices:
+          arestas = self.corpo[vertice]
+          lista += f"{vertice}: "
+          for i, (vizinho, peso) in enumerate(arestas):
+              seta = "-> " if i != len(arestas) - 1 else ""
+              lista += f"({vizinho}, {peso}) {seta}"
+          lista += "\n"
+      return lista
 
-  def add_aresta(self,vertice1, vertice2, peso):
-    if peso < 0:
-      raise ValueError("Peso inválido!")
-    else:
-      if vertice1 not in self.corpo:
-            self.add_vertice(vertice1)
-      if vertice2 not in self.corpo:
-        self.add_vertice(vertice2)
+  def add_aresta(self, vertice1, vertice2, peso):
+      if peso < 0:
+          raise ValueError("Peso inválido!")
+      
+      # Adiciona vértices se não existirem
+      if vertice1 not in self.vertices:
+          self.add_vertice(vertice1)
+      if vertice2 not in self.vertices:
+          self.add_vertice(vertice2)
 
+      # Verifica se aresta existe em qualquer direção
       if self.tem_aresta(vertice1, vertice2):
-        #print("Aresta já existe! Peso será mudado")
-        for i in range(len(self.corpo[vertice1])):
-          if self.corpo[vertice1][i][0] == vertice2:
-            self.corpo[vertice1][i][1] = peso
-            return
-
-      if not self.tem_aresta(vertice1, vertice2):
-        self.corpo[vertice1].append([vertice2, peso])
-        self.tamanho += 1
+          # Atualiza peso se existir na direção vertice1->vertice2
+          for i in range(len(self.corpo[vertice1])):
+              if self.corpo[vertice1][i][0] == vertice2:
+                  self.corpo[vertice1][i][1] = peso
+                  return
+      else:
+          self.corpo[vertice1].append([vertice2, peso])
+          self.tamanho += 1
 
   def print_grafo(self):
-      for vertice in self.corpo:
+      for vertice in self.vertices:
           print(vertice, ":", self.corpo[vertice])
       print("Tamanho do grafo:", self.tamanho)
       print("Ordem do grafo:", self.ordem)
 
   def remove_aresta(self, vertice1, vertice2):
-    if vertice1 in self.corpo:
-      for i in range(len(self.corpo[vertice1])):
-        if self.corpo[vertice1][i][0] == vertice2:
-          self.corpo[vertice1].pop(i)
-          self.tamanho -= 1
-          break
+      if vertice1 in self.vertices:
+          for i in range(len(self.corpo[vertice1])):
+              if self.corpo[vertice1][i][0] == vertice2:
+                  self.corpo[vertice1].pop(i)
+                  self.tamanho -= 1
+                  return
 
   def remove_vertice(self, vertice):
-    if vertice in self.corpo:
-      for v in self.corpo:
-        if self.tem_aresta(vertice, v):
-          self.remove_aresta(vertice, v)
-        self.remove_aresta(v, vertice)
-      self.corpo.pop(vertice)
-      self.ordem -= 1
-    else:
-      raise ValueError("Vértice não existe!")
+      if vertice in self.vertices:
+          # Remove todas as arestas conectadas
+          for v in self.vertices:  # Cópia para iterar com segurança
+            if self.tem_aresta(vertice, v):
+              self.remove_aresta(vertice, v)
+            self.remove_aresta(v, vertice)
+          self.vertices.remove(vertice)
+          del self.corpo[vertice]
+          self.ordem -= 1
+      else:
+          raise ValueError("Vértice não existe!")
 
   def tem_aresta(self, vertice1, vertice2):
-    if vertice1 in self.corpo and vertice2 in self.corpo:
+      if vertice1 in self.vertices and vertice2 in self.vertices:
         for vizinho, _ in self.corpo[vertice1]:
-            if vizinho == vertice2:
-                return True
+          if vizinho == vertice2:
+              return True
         for vizinho, _ in self.corpo[vertice2]:
             if vizinho == vertice1:
                 return True
         return False  
-    else:
-        raise ValueError("Pelo menos um desses vértices não existe!")
+      raise ValueError("Vértice não existe!")
 
   def grau_entrada(self, vertice):
     grau_entrada = 0
-    if vertice in self.corpo:
-      for v in self.corpo:
-        if len(self.corpo[v]) > 0:
-          for i in range(len(self.corpo[v])):
-            if self.corpo[v][i][0] == vertice:
-              grau_entrada += 1
+    if vertice in self.vertices:
+      for v in self.vertices:
+        grau_entrada += len([vizinho for vizinho,_ in self.corpo[v] if vizinho == vertice])
       return grau_entrada
     else:
       raise ValueError("Vértice não existe")
-
+    
   def grau_saida(self, vertice):
-    if vertice in self.corpo:
+      if vertice not in self.vertices:
+          raise ValueError("Vértice não existe")
       return len(self.corpo[vertice])
-    else:
-      raise ValueError("Vértice não existe")
 
   def grau(self, vertice):
-    return self.grau_entrada(vertice) + self.grau_saida(vertice)
+      return self.grau_entrada(vertice) + self.grau_saida(vertice)
 
-  def get_peso(self, vertice1, vertice2):
-    if vertice1 in self.corpo:
-     for i in range(len(self.corpo[vertice1])):
-        if len(self.corpo[vertice1]) > 0:
-           if self.corpo[vertice1][i][0] == vertice2:
-            return self.corpo[vertice1][i][1]
-    else:
-      raise ValueError("Vértice não existe")
-           
+  def get_peso(self, vertice1, vertice2):      
+      if vertice1 not in self.vertices:
+          raise ValueError("Vértice não existe")
+      for vizinho, peso in self.corpo[vertice1]:
+          if vizinho == vertice2:
+              return peso
+      return None
+
   def salva_grafo(self, caminho):
-    with open(caminho,'w') as f:
-      for vertice in self.corpo:
-        dados = []
-        if len(self.corpo[vertice]) > 0:
-          for i in range(len(self.corpo[vertice])):
-              dados.append(self.corpo[vertice][i])
-              dados_formatados = [f'{d} ->' for d in dados]
-          f.write(f'{vertice}:{dados_formatados}\n')
-        else:
-          f.write(f'{vertice}:\n')
-      print("Grafo salvo com sucesso!")
+      with open(caminho,'w') as f:
+          for vertice in self.vertices:
+              adjacentes = self.corpo[vertice]
+              if adjacentes:
+                  dados_formatados = [f'{aresta} ->' for aresta in adjacentes]
+                  f.write(f'{vertice}:{dados_formatados}\n')
+              else:
+                  f.write(f'{vertice}:\n')
+          print("Grafo salvo com sucesso!")
 
   def carrega_grafo(self, caminho):
-     with open(caminho, 'r') as f:
-        for linha in f:
-          remetente, conteudo = linha.split(":", 1)  
+      with open(caminho, 'r') as f:
+          for linha in f:
+              remetente, conteudo = linha.split(":", 1)
+              destinatarios = re.findall(r"'(.*?)', (\d+)", conteudo)
+              emails = {remetente: [(email, int(numero)) for email, numero in destinatarios]}
+              
+              if emails[remetente]:
+                  for destinatario in emails[remetente]:
+                      pessoa, peso = destinatario
+                      self.add_aresta(remetente, pessoa, peso)
+              else:
+                  try:
+                      self.add_vertice(remetente)
+                  except:
+                      pass
 
-          # '(.*?)' -> pega qualquer coisa entre '' 
-          # (\d+) -> pega qualquer número decimal
-          destinatarios = re.findall(r"'(.*?)', (\d+)", conteudo)
-
-          #faço um list comprehension para criar um dic com a estrutura: Remetente: [pessoa, peso], [pessoa, peso] , ...
-          emails = {remetente: [(email, int(numero)) for email, numero in destinatarios]}
-
-          #caso o remetente não esteja vazio, vou percorer a lista de pessoas e ir add
-          if emails[remetente]:
-            for destinatario in emails[remetente]:
-              pessoa, peso = destinatario
-              self.add_aresta(remetente, pessoa, peso)
-          else:
-            try:
-              #add os vertíces que não tinham conexões saindo dele
-              self.add_vertice(remetente)
-            except:
-               pass
-            
   def vertices_isolados(self):
-    isolados = []
-    for vertice in self.corpo:
-      if self.grau(vertice) == 0:
-        isolados.append(vertice)
-    
-    print(f"Os grafos isolados são: {isolados}")
+      isolados = [v for v in self.vertices if self.grau(v) == 0]
+      print(f"Os vértices isolados são: {isolados}")
+
+  # Demais métodos mantidos com lógica similar...
 
   #Implemente um método que retorne uma lista com todos os vértices que
   #estão localizados até uma distância D de um vértice N, em que D é a soma dos
@@ -206,22 +198,22 @@ class Grafo:
   def teste(self, distancia, vertice):
     pass
 
-    def euleriano(self): 
-      vertices_eulerianos = []
-      for vertice in self.corpo:
-        if (self.grau(vertice) % 2 == 0) and (self.grau_entrada(vertice) == self.grau_saida(vertice)):
-          if vertice not in vertices_eulerianos:
-            vertices_eulerianos.append(vertice)
-        else: 
-          return False
-      for vertice in vertices_eulerianos:
-        for i in range(self.grau_saida(vertice)):
-          last_vertice = vertice
-          current_vertice = 0 
-          while current_vertice != vertice:
-            for i in range(self.gra)
-            current_vertice = self.corpo[last_vertice][current_vertice][]
-      print(f"Os grafos eulerianos são: {vertices_eulerianos}")
+    # def euleriano(self): 
+    #   vertices_eulerianos = []
+    #   for vertice in self.corpo:
+    #     if (self.grau(vertice) % 2 == 0) and (self.grau_entrada(vertice) == self.grau_saida(vertice)):
+    #       if vertice not in vertices_eulerianos:
+    #         vertices_eulerianos.append(vertice)
+    #     else: 
+    #       return False
+    #   for vertice in vertices_eulerianos:
+    #     for i in range(self.grau_saida(vertice)):
+    #       last_vertice = vertice
+    #       current_vertice = 0 
+    #       while current_vertice != vertice:
+    #         for i in range(self.gra)
+    #         current_vertice = self.corpo[last_vertice][current_vertice][]
+    #   print(f"Os grafos eulerianos são: {vertices_eulerianos}")
 
   
 
